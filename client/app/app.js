485835a981be47c52c12a4bcb4787ec0ef72ab8d
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2020-03-21 23:57:58 -0700

    setup webpack & react

---
+++ client/app/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div>React Webpack Starter</div>;
+};
+
+export default App;
---
+++ client/app/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app';
+
+// // Import application sass styles
+// import './styles/sass/style.scss';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<App />, rootElement);
